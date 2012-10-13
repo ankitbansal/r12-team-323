b7663790cfe9cc7e369d818bac0b7c8a8601329f
Ankit <ankitba@Macintosh-2.local>
2012-10-13 23:51:15 +0100

    redirecting to projects page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     session[:token] = auth['credentials'][:token]
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to projects_path(user), :notice => "Signed in!"
   end
   
   def destroy

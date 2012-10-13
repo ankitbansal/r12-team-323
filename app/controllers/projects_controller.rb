12f1a8589f596b44f52f9014a6bdf9e7ed382a02
Ankit <ankitba@Macintosh-2.local>
2012-10-13 13:16:03 +0100

    Added new projects functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,33 @@
 class ProjectsController < ApplicationController
   
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+
+       respond_to do |format|
+         if @project.save
+           format.html { redirect_to(@project, :notice => 'Project was successfully created.') }
+           format.xml { render :xml => @project, :status => :created, :location => @project }
+         else
+           format.html { render :action => "new" }
+           format.xml { render :xml => @project.errors, :status => :unprocessable_entity }
+         end
+       end
+  end
+  
+  def index
+    @projects = Project.all
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+
+      respond_to do |format|
+          format.html # show.html.erb
+          format.xml { render :xml => @project }
+      end
+  end
 end

d554da24cf66e61538dac58246b94c41d3dc0a53
Ramanathan Balakrishnan <ramanab@default.corporate.thoughtworks.com>
2012-10-13 12:31:22 +0100

    Friends as a resource

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ end
 
 gem 'jquery-rails'
 gem 'jquery_mobile_rails'
+gem 'koala'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 R12Team323::Application.routes.draw do
+  resources :friends
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

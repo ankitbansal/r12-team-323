d9d2f52c896a6e173e03aeaeadb0c72d5dfb7c28
Jatin Naik <jsnaik@gmail.com>
2012-10-14 00:48:17 +0100

    Corrected db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20121013213625) do
+ActiveRecord::Schema.define(:version => 20121013232535) do
 
   create_table "comments", :force => true do |t|
     t.string   "title",            :limit => 50, :default => ""

11c8c140685a01be3a1a487427e5d60aac41c16c
Jatin Naik <jsnaik@gmail.com>
2012-10-14 13:07:28 +0100

    not redirecting f dev

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    redirect_to :promo and return if(!mobile_device?)
+    redirect_to :promo and return if(!mobile_device?) && Rails.env != "development"
     redirect_to projects_path(current_user) if current_user
   end
 

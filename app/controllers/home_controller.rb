97d78896a9a17eb784591da1504cbe7f107c2cee
Ramanathan Balakrishnan <ramanab>
2012-10-14 18:36:51 +0000

    Styled the landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     redirect_to :promo and return if(!mobile_device?) && Rails.env != "development"
     redirect_to projects_path(current_user) if current_user
+    render :layout => 'index'
   end
 
   def promo
